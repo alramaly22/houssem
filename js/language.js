49036dfa1a4105be113a62251cca471f6cdacd94
m.alramaly <96423919+alramaly22@users.noreply.github.com>
2024-10-15 08:56:53 +0300

    Add files via upload

---
+++ js/language.js
@@ -0,0 +1,330 @@
+let lang = {
+    ar: {
+        nav1: "الرئيسية",
+        nav2: "عن المدرب",
+        nav3: "التحولات",
+        nav4: "الأسعار",
+        nav5: "تواصل",
+        home1: "استكشف إمكانياتك مع حسام فاروق",
+        button1: "خدماتنا",
+        homep1: "سنوات من",
+        homep2: "الخبرة",
+        homeh31: "ذهبية",
+        homep3: "مستر أولمبيا ماسترز",
+        homep4: "لقب الوزن المفتوح",
+        homeh32: "برونزية",
+        homep5: "مستر أولمبيا ماسترز",
+        homep6: "ميدالية 212",
+        serv1: "كيف تبدأ",
+        servh31: "تعرف على المدرب",
+        servp1: "ابدأ بقراءة كل المعلومات عن المدرب حسام فاروق، بما في ذلك إنجازاته وخبرته الواسعة.",
+        servh32: "تحقق من قصص النجاح",
+        servp2: "استعرض قصص نجاح المتدربين. وتشمل الصور والشهادات للرياضيين.",
+        servh33: "اختر باقتك",
+        servp3: "اختر الباقة المناسبة لأهدافك، وابدأ رحلتك نحو النجاح.",
+
+        about1: "حول",
+        about2: "حسام",
+        about3: "حسام فاروق، مع 29 عامًا من الخبرة، يعد من أفضل مدربي كمال الأجسام المعروفين بإنجازاته وبرامج التدريب التحويلية. سواء كنت تهدف إلى فقدان الوزن، زيادة العضلات، أو اللياقة العامة، حسام هنا لمساعدتك على النجاح.",
+        buttonabout2: "اعرف المزيد",
+
+        abop1: "المزيد عن",
+        abop2: "حسام",
+        abop3: "المدرب حسام فاروق، جزائري الأصل ويحمل الجنسية التونسية، يعيش في الإمارات منذ 25 عامًا. حاصل على درجة في الرياضة والتربية البدنية، بالإضافة إلى شهادات تدريب كمال الأجسام من أكاديمية جو ويدر والتغذية من أكاديمية فليكس. مع 29 عامًا من الخبرة، حقق العديد من الإنجازات منها الفوز بالميدالية الذهبية ولقب مستر أولمبيا ماسترز فئة الوزن المفتوح، بالإضافة إلى الميدالية البرونزية في فئة 212 في مستر أولمبيا ماسترز. كما حصل على لقب الوزن المفتوح للماسترز في رومانيا بجانب كمال الجارني. يقوم بتدريب المشاهير، السياسيين، والنخبة، ويقدم التدريب الشخصي وعبر الإنترنت للإعداد للمسابقات وتحقيق أهداف اللياقة البدنية.",
+        home21: "أطلق إمكانياتك الحقيقية مع حسام فاروق",
+        transformation: "التحولات والإنجازات",
+
+        pricing_title: "الأسعار",
+        natural_package: "الباقة الأساسية",
+        natural_packagep: "$300",
+        month1: "شهر واحد",
+        ulp1: "خطة تدريب كاملة",
+        ulp2: "مكملات غذائية مشمولة",
+        ulp3: "إرشادات التغذية",
+        ulp4: "تخطيط الدورة التدريبية",
+        ulp5: "التنظيف والحماية",
+        ulp6: "نصائح تدريبية مخصصة",
+        ulp7: "نصائح التغذية",
+        ulp8: "تدريب فردي",
+        ulp9: "تمارين مخصصة",
+        get_started: "ابدأ الآن",
+        recommend: "موصى به",
+        competition: "الباقة المميزة",
+        natural_packagep2: "$1500",
+        month2: "6 أشهر",
+        competition_prep: "الباقة النهائية",
+        natural_packagep3: "$2500",
+        month3: "1 سنة",
+        consultation: "استشارة",
+        consultation_price: "$150",
+        session: "لكل جلسة",
+        personal_trainer: "مدرب شخصي",
+        personal_trainer_price: "$150",
+        session2: "جلسة وجهاً لوجه",
+
+        questionp1: "الأسئلة الشائعة حول",
+        questionp2: "التدريب مع المدرب",
+        q1: "كيف سيساعدني التدريب مع المدرب على تحقيق أهدافي؟",
+        q2: "هل يمكنني حقًا المنافسة على مستوى عالٍ مع تدريب المدرب؟",
+        q3: "كيف يدعمني المدرب خلال رحلتي؟",
+        q4: "ما هي النتائج التي يمكنني توقعها؟",
+        q5: "كيف يختلف برنامج المدرب عن البرامج الأخرى؟",
+        q6: "هل تدريب المدرب مناسب للمبتدئين؟",
+        a1: "التدريب مع المدرب مصمم لتوجيهك خطوة بخطوة، مما يضمن وصولك إلى أهداف اللياقة بكفاءة. سواء كنت تهدف إلى جسم البطولة أو مجرد تحسين لياقتك، سيخصص المدرب الخطة حسب احتياجاتك، مع دمج التمارين، التغذية، والمكملات.",
+        a2: "بالتأكيد! المدرب لديه سجل حافل في إعداد الرياضيين للمنافسات. مع التوجيه الشخصي في كل جانب، من النظام الغذائي إلى برامج التمرين، ستحصل على الدعم اللازم لتقديم أفضل أداء والفوز بالميداليات الذهبية.",
+        a3: "يقدم المدرب تواصل يومي عبر WhatsApp للتأكد من أنك تسير على المسار الصحيح، ويقوم بتعديل برنامجك عند الحاجة، ويقدم لك الدافع المستمر. لن تشعر أبدًا بأنك وحدك في رحلتك اللياقية!",
+        a4: "مع الالتزام والمثابرة، ستلاحظ تحسينات في جسمك وقوتك ولياقتك العامة. برامج المدرب مصممة لتحقيق تحولات على المدى القصير والطويل، مما يضمن تحقيقك لجسم أحلامك سواء للمنافسات أو الأهداف الشخصية.",
+        a5: "المدرب يقدم نهجًا شاملاً، لا يشمل فقط خطط التمارين بل أيضًا إعداد الوجبات، المكملات، وإدارة الدورة. هذا النهج الشامل يضمن أنك مستعد جسديًا ونفسيًا لأي تحدي لياقي، سواء في الحياة اليومية أو في الرياضات التنافسية.",
+        a6: "نعم! المدرب يخصص البرنامج حسب مستوى خبرتك، مما يضمن أن المبتدئين والمحترفين على حد سواء يمكنهم التقدم بفعالية. مع توجيهات المدرب، ستبني أساسًا قويًا وتعمل بانتظام لتحقيق أهداف اللياقة النهائية الخاصة بك.",
+
+        contact: "تواصل معي",
+        sendmessage: "إرسال رسالة",
+
+        footerhome: "الرئيسية",
+        footerpricing: "الأسعار",
+        footerabout: "حول",
+
+        clients1: "أنا سعيد جدًا بالبرنامج وسعيد أنني اتبعت إرشاداتك خلال التحضير. كانت لدي تجارب سيئة مع مدربين آخرين قبلك، حتى بعض الأصدقاء. أنت محترف حقيقي وتظهر احترامًا كبيرًا. الأمر ليس فقط بشأن المال بالنسبة لك؛ بل تركز على الجودة بشكل حقيقي. بارك الله فيك.",
+        clients2: "في هذه الصور، يوجد جائزتان. الأولى هي الميدالية الذهبية. هذه الكلمات تجعلني فخورًا جدًا، خاصة عندما تأتي من أفضل شخص قابلته في حياتي، مدربي وأخي، الدكتور محمد القاضي. لا أستطيع أن أعبر عن مدى تقديري لما قدمه لي. الصورة الثانية هي مجرد ميدالية 🥇. شكراً لكل من دعموني. شكرًا لكم جميعًا.",
+        clients3: "أنت أفضل مدرب تعاملت معه، والله. أنت مثل الطبيب والفنان في كمال الأجسام. أنا راضٍ وسعيد جدًا. أدعو لك ليل نهار. أينما ذهبت، أتحدث عنك بفخر. أوصي كل أصدقائي بالتدريب معك. بإذن الله سأواصل العمل معك.",
+        clients4: "حصلت على المركز الأول في الفيزيك الرجال وحصلت على بطاقة الفيزيك الاحترافية. وحصلت على المركز الثاني في كمال الأجسام. كان الأمر قريبًا جدًا بيني وبين المنافس الآخر. أنا سعيد جدًا بأنني عملت معك، صديقي. شكراً لك، حققت كل هذا بفضلك.",
+        nameclients1: "فهد علي",
+        nameclients2: "ماجد سعيد",
+        nameclients3: "يوسف حسن",
+        nameclients4: "خالد أحمد",
+
+        home31: "حقق أفضل نتائجك",
+        home32: "أطلق قوتك",
+
+        cardtitle16: "تحدث معنا",
+        cardtitle15: "إرسال لقطة الشاشة",
+        cardtitle14: "الخطوة 3",
+        cardtitle13: "التقاط لقطة شاشة",
+        cardtitle12: "الخطوة 2",
+        cardtitle11: "حسام بن ضيف الله",
+        cardtitle10: "الدفع عبر ويسترن يونيون، تحويل الرسوم إلى",
+        cardtitle9: "الخطوة 1",
+        cardtitle8: "ويسترن يونيون",
+        cardtitle7: "إرسال لقطة الشاشة",
+        cardtitle6: "الخطوة 3",
+        cardtitle5: "التقاط لقطة شاشة",
+        cardtitle4: "الخطوة 2",
+        cardtitle3: "الدفع عبر الأنصاري للصرافة، تحويل الرسوم إلى",
+        cardtitle2: "الخطوة 1",
+        cardtitle1: "الأنصاري للصرافة"
+
+    },
+    en: {
+        nav1: "Home",
+        nav2: "About",
+        nav3: "Trasformation",
+        nav4: "Pricing",
+        nav5: "Contact",
+        nav6: "Clients",
+        nav7: "Faqs",
+
+        home1: "Unleash your true potential with Hussam Farouk",
+        button1: "Our Services",
+        homep1: "Years of",
+        homep2: "Experience",
+        homeh31: "Gold",
+        homep3: "Mr. Olympia Masters",
+        homep4: "Open Weight Title",
+        homeh32: "Bronze",
+        homep5: "Mr. Olympia Masters",
+        homep6: "212 Medal",
+
+        home21: "Unleash your true potential with Hussam Farouk",
+        button3: "Our Services",
+
+        serv1: "How to Get Started",
+        servh31: "Learn About the Coach",
+        servp1: "Start by reading all the information about Coach Hussam Farouk, including his achievements and extensive experience in bodybuilding and fitness training.",
+        servh32: "Check Success Stories",
+        servp2: "Explore the success stories of athletes trained by the coach. These include images and testimonials of champions who have reached the top under his guidance.",
+        servh33: "Choose Your Package",
+        servp3: "Select the training package that suits your needs and goals. Once chosen, click on 'Get Started' to begin your journey towards success.",
+
+        about1: "About",
+        about2: "Houssem",
+        about3: "Hussam Farouk, with 29 years of experience, is a top bodybuilding coach known for his achievements and transformative training programs. Whether your goal is weight loss, muscle gain, or overall fitness, Hussam is here to help you succeed.",
+        buttonabout2: "Know More",
+
+        abop1: "More About",
+        abop2: "Houssem",
+        abop3: "Coach Houssam Farouk, originally Algerian with Tunisian nationality, has lived in the UAE for 25 years. He holds a degree in sports and physical education, along with certifications in bodybuilding coaching from Joe Weider Academy and nutrition from Flexs Academy. With 29 years of experience, his major achievements include winning the Gold Medal and the Mr. Olympia Masters Open Weight Title, alongside a Bronze Medal in the 212 category at Mr. Olympia Masters. He also secured the Masters Open title in Romania, alongside Kamal Elgargni. He trains celebrities, politicians, and elites, offering both in-person and online coaching for competition prep and fitness goals.",
+
+        transformation: "Transformations & Triumphs",
+
+        pricing_title: "PRICING",
+        natural_package: "Basic Package",
+        natural_packagep: "$300",
+        month1: "1 Month",
+        ulp1: "Full Training Plan",
+        ulp2: "Supplements Included",
+        ulp3: "Nutrition Guidance",
+        ulp4: "Cycle Planning",
+        ulp5: "Cleaning & Protection",
+        ulp6: "Personalized Training Advice",
+        ulp7: "Nutrition Tips",
+        ulp8: "One-on-One Training",
+        ulp9: "Tailored Workouts",
+        get_started: "Get Started",
+        recommend: "Recommended",
+        competition: "Premium Package",
+        natural_packagep2: "$1500",
+        month2: "6 Months",
+        competition_prep: "Ultimate Package",
+        natural_packagep3: "$2500",
+        month3: "1 Year",
+        consultation: "Consultation",
+        consultation_price: "$150",
+        session: "Per Session",
+        personal_trainer: "Personal Trainer",
+        personal_trainer_price: "$150",
+        session2: "Face-to-Face Session",
+
+        questionp1: "Common Questions About",
+        questionp2: "Training with Coach",
+        q1: " How will training with the coach help me achieve my goals?",
+        q2: "Can I really compete at a high level with the coach's training?",
+        q3: "How does the coach support me throughout my journey?",
+        q4: " What kind of results can I expect?",
+        q5: "How is the coach's program different from others?",
+        q6: "Is the coach's training suitable for beginners?",
+        a1: "   Training with the coach is designed to guide you step by step, ensuring you reach your fitness goals efficiently. Whether you aim for a championship physique or just want to transform your body, the coach tailors the plan to your needs, incorporating workouts,nutrition,and supplements.",
+        a2: "Absolutely! The coach has a proven track record of preparing athletes for competition. With personalized guidance in every aspect, from diet to workout regimes, you'll have the support you need to perform at your best and win those gold medals.",
+        a3: "The coach provides daily communication via WhatsApp to ensure you're on track, adjusting your program when needed, and offering constant motivation. You'll never feel alone on your fitness journey!",
+        a4: "With consistency and dedication, you'll notice improvements in your physique, strength, and overall fitness. The coach's programs are designed for both short-term and long-term transformations, ensuring you achieve your desired body, whether for competitions or personal goals.",
+        a5: "The coach offers a comprehensive approach, including not only workout plans but also meal prep, supplements, and cycle management. This holistic method ensures you are physically and mentally prepared for any fitness challenge, from daily life to competitive sports.",
+        a6: "Yes! The coach tailors the program to your level of experience, ensuring that beginners and advanced athletes alike can progress effectively. With the coach's guidance, you'll build a strong foundation and steadily work toward your ultimate fitness goals.",
+
+        contact: "Contact Me",
+        sendmessage: "Send Message",
+
+        footerhome: "Home",
+        footerpricing: "Pricing",
+        footerabout: "About",
+
+        clients1: "I'm truly happy with the program and glad I followed your guidance during the preparation. I had some bad experiences with other trainers before you, even some friends. You're a real pro and show great respect. It's not just about money for you; you genuinely focus on quality.Bless you.",
+        clients2: "In these pictures, there are two trophies. The first is the gold medal. These words make me very proud, especially coming from the best person I've ever met, my coach and brother, Dr. Mohammed El Gadi Mo Qadi. I can't express how much I appreciate what he 's done for me. The second picture is just a medal 🥇. Thanks to everyone who supported me. Thank you all ",
+        clients3: "You're the best coach I've ever worked with, I swear. You're like a doctor and an artist in bodybuilding. I'm really satisfied and happy. I pray for you day and night. Wherever I go, I speak highly of you. I recommend all my friends to train with you. God willing, I'll continue working with you.",
+        clients4: "I took the first overall in men's physique and earned a physique pro card. I came second in overall bodybuilding. It was really close between me and the other dude. I'm very happy that I worked with you, my friend. Thanks to you, I achieved all of this",
+        nameclients1: "Fahd Ali",
+        nameclients2: "Majed Saeed",
+        nameclients3: "Youssef Hassan",
+        nameclients4: "Khaled Ahmed",
+
+
+        home31: "Achieve Your Best",
+        home32: "Unleash Your Strength",
+
+
+        cardtitle16: "Chat with us",
+        cardtitle15: "SEND SCREENSHOOT",
+        cardtitle14: "STEP 3",
+        cardtitle13: "Take A Screenshot",
+        cardtitle12: "STEP 2",
+        cardtitle11: "Houssem Ben Dhifallah",
+        cardtitle10: "Pay With Western Union Transfer The Fees To",
+        cardtitle9: "STEP 1",
+        cardtitle8: "Western Union",
+        cardtitle7: "SEND SCREENSHOOT",
+        cardtitle6: "STEP 3",
+        cardtitle5: "Take A Screenshot",
+        cardtitle4: "STEP 2",
+        cardtitle3: "Pay With AL ANSARI EXCHANGE Transfer The Fees To",
+        cardtitle2: "STEP 1",
+        cardtitle1: "AL ANSARI EXCHANGE",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+        nav1: "",
+    }
+};
+
+let selector = document.getElementById('language-selector');
+
+// استدعاء الدالة عند تغيير اللغة
+selector.addEventListener('change', updateLanguage);
+
+// استدعاء الدالة لتعيين اللغة الافتراضية عند تحميل الصفحة
+updateLanguage();
+
+function updateLanguage() {
+    let language = selector.value; // اللغة المختارة
+    let nodes = document.querySelectorAll('[data-lang]');
+
+    // تحديث نصوص العناصر باستخدام اللغة المناسبة
+    nodes.forEach(node => {
+        let key = node.getAttribute('data-lang');
+        if (lang[language] && lang[language][key]) {
+            node.textContent = lang[language][key];
+        }
+
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('arabic-font');
+        } else {
+            node.classList.remove('arabic-font');
+            node.classList.add('english-font');
+        }
+
+        // إضافة الفئة الإضافية هنا (مثل 'second-arabic-font') إذا كان ذلك مطلوبًا
+        // وتبديلها بناءً على اللغة المحددة بنفس الطريقة
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('second-arabic-font');
+        } else {
+            node.classList.remove('second-arabic-font');
+            node.classList.add('english-font');
+        }
+        if (language === 'ar') {
+            node.classList.remove('english-font');
+            node.classList.add('se-arab');
+        } else {
+            node.classList.remove('se-arab');
+            node.classList.add('english-font');
+        }
+    });
+
+    // تحديث اتجاه النص في الجسم بناءً على اللغة
+    document.body.style.direction = language === 'ar' ? 'rtl' : 'ltr';
+    document.body.style.textAlign = language === 'ar' ? 'right' : 'left';
+
+    // التأكد من أن قسم "tranding" يظل بخصوصيته
+    let trandingSection = document.getElementById('new');
+    if (trandingSection) {
+        trandingSection.style.direction = 'ltr';
+        trandingSection.style.textAlign = 'left';
+    }
+    let projectsSection = document.getElementById('travel');
+    if (projectsSection) {
+        projectsSection.style.direction = 'ltr';
+        projectsSection.style.textAlign = 'left';
+    }
+
+    let testimonialSection = document.getElementById('clients');
+    if (testimonialSection) {
+        testimonialSection.style.direction = 'ltr';
+        testimonialSection.style.textAlign = 'left';
+    }
+
+
+}
